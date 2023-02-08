9c4426feeddab9781fac0e783bd0896b52f99e3b
Erick Uresti <erick22uresti@gmail.com>
2023-02-07 18:09:45 -0600

    Exercise 3 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(message) {
+    const size = message.length;
+    let reverse = "";
+    for(let i = size; i >= 0; i--){
+        reverse += message.charAt(i)
+    }
 
+    return reverse;
 };
 
 // Do not edit below this line
