ea9a8e45f3a6fa1a0f2d1f22b84af9200b8d4c95
Erick Uresti <erick22uresti@gmail.com>
2023-02-07 18:01:53 -0600

    Exercise 2 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function (message, number) {
+  let string = "";
 
+  if(number < 0){
+    return "ERROR"
+  }
+
+  for (let i = 0; i < number; i++) {
+    string += message;
+  }
+
+  return string;
 };
 
 // Do not edit below this line
