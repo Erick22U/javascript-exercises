b8b1a6da258bb1362da35200ec38991d2176c61f
Erick Uresti <erick22uresti@gmail.com>
2023-07-14 14:30:15 -0500

    Finished palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+const palindromes = function (message) {
 
+    message = message.toLowerCase()
+
+    let messageArray = message.split("")
+    messageArray = messageArray.filter(letter => letter.match(/[a-z]/i))
+
+    let isPalindrome = true;
+
+    let i = 0;
+    let j = messageArray.length - 1;
+    let mid = messageArray.length / 2;
+
+    for(i; i < mid; i++){
+        if(messageArray[i] != messageArray[j]) return false;
+        j--;
+    }
+
+    return isPalindrome
 };
 
 // Do not edit below this line
