efb276c74b7d8b883f352e53dd5f43784d8b89a6
Erick Uresti <erick22uresti@gmail.com>
2023-07-14 11:22:51 -0500

    Finished Calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let total = 0;
+
+	if(arr.length == 0) return 0;
+
+  if(arr.length == 1) return arr[0];
+
+  return arr.reduce((total, amount) => total + amount)
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((total, amount) => total * amount, 1)
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return Math.pow(base, exponent)
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if(num == 0) return 1;
+
+  let product = 1;
+
+  for(let i = num; i > 0; i--) product = product * i
+  return product
 };
 
 // Do not edit below this line
