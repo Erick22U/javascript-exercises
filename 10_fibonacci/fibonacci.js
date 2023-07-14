58a248f7ee1f48252466b20e9b6672efc55bf9f5
Erick Uresti <erick22uresti@gmail.com>
2023-07-14 14:45:00 -0500

    Finished fibonocci sequence

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(count) {
 
+    if(count < 0) return "OOPS"
+
+    if(count == 0) return 0;
+
+    if(count == 2 || count == 3) return 1;
+
+    let curCount = 2;
+
+    let cur = 1;
+    let prev = 1;
+    let sum = 0;
+
+    for(curCount; curCount < count; curCount++) {
+        sum = cur + prev;
+        prev = cur;
+        cur = sum;
+    }
+    return cur;
 };
 
 // Do not edit below this line
